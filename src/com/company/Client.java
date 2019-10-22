64009873c82f6e43e49f501dc4ab644486b3682b
Clement Decoopman <decoopmc@ens-epua.univ-savoie.fr>
2019-10-22 17:03:14 +0200

    add some comments

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -8,9 +8,10 @@ import java.rmi.registry.Registry;
 public class Client {
     public static void main(String[] argv) {
         try {
-            Registry registry = LocateRegistry.getRegistry(10000);                       // acces au registre
-            SwitcherInterface stub = (SwitcherInterface) registry.lookup("Switcher");    // choix de l'instance
-            stub.check();                                                                // fonction appellee cote serveur
+            Registry registry = LocateRegistry.getRegistry(10000);                       // acces au registre sur le port 10000
+            SwitcherInterface stub = (SwitcherInterface) registry.lookup("Switcher");    // choix de l'instance > on recherche l'object "switcher" qui a été bind
+            stub.check();                     // fonction appellee cote serveur
+
         } catch (Exception e) {
             e.printStackTrace();
         }
